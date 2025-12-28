45910bb6928345416c103b2973f5867d885fa83f
evgeny.l <marsmaer@gmail.com>
2025-12-28 15:55:07 +0300

    added <y> to main

---
+++ main.py
@@ -0,0 +1,15 @@
+# git remote add origin https://github.com/Evgeny725/ml-01.git
+# git branch -M main
+# git push -u origin main
+
+print("hello world")
+
+x=1+1
+print(x)
+
+# new comment to make changes
+
+y=2+2
+print(y)
+
+
