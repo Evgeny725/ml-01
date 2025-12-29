0304540f84161dd6208cf3f15d6731911f29d0fa
evgeny.l <marsmaer@gmail.com>
2025-12-29 18:48:29 +0300

    updated main

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
 # git remote add origin https://github.com/Evgeny725/ml-01.git
 # git branch -M main
 # git push -u origin main
+import string
+from decimal import Decimal
 
 print("hello world")
 
-x=1+1
+x = 1 + 1
 print(x)
 
-# new comment to make changes
 
-y=2+2
-print(y)
+a = Decimal('0.56')
+print(a)
 
-# added changes via browser UI
+# use example below to declare infinite value
+b = float('inf')
+print(b)
 
-z=5
+# to represent a number as hex value and vice versa
+
+hex(123456)
+print(hex(123456))
+
+d = int('1e240',16)
+print(d)
+
+# truncate string <strip>, from right end <rstrip>, from left end <lstrip>
+str1 = '  abcdef     '
+str2 = str1.strip(' ')
+print(str2)
+
+# string
+name = 'abc'
+age = 123
+string = f'name is {name} and age is {age}'
+print(string)
+
+# cascade call
+str3 = '  ABC DEF GHI '
+str4 = str3.strip(' ').lower().split()
+print(str4)
+
